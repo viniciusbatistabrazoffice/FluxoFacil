d34f4f3319a8d6103f0e62808e7a68d2116a3bd6
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-18 11:27:02 -0300

    Implementando os metodos de crud vazio em conta service impl

---
+++ backend/src/main/java/com/backend/service/ContaServiceImpl.java
@@ -0,0 +1,46 @@
+package com.backend.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.backend.entity.Conta;
+
+public class ContaServiceImpl implements ContaService {
+
+    @Override
+    public void criarConta(String titular) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'criarConta'");
+    }
+
+    @Override
+    public void deletarConta(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deletarConta'");
+    }
+
+    @Override
+    public void atualizarConta(Long id, String titular) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarConta'");
+    }
+
+    @Override
+    public void atualizarSaldo(Long id, BigDecimal saldo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'atualizarSaldo'");
+    }
+
+    @Override
+    public Conta getContaById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getContaById'");
+    }
+
+    @Override
+    public List<Conta> getAllContas() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllContas'");
+    }
+    
+}
