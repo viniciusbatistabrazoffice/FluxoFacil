24700572a6ee51129cdb1958d094df44b23c1fb5
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-18 12:20:37 -0300

    Implementando security config

--- backend/src/main/java/com/backend/repository/UsuarioRepository.java
+++ backend/src/main/java/com/backend/repository/UsuarioRepository.java
@@ -1,9 +1,14 @@
 package com.backend.repository;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.entity.Usuario;
 
-
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
---
+++ backend/src/main/java/com/backend/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(
+                    SessionCreationPolicy.STATELESS
+                )
+            )
+
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
