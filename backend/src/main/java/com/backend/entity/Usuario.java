f2fe4e99c62b1eadfb71a6f00971aa963e238a28
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-18 12:03:13 -0300

    Implementando a entidade usuario

---
+++ backend/src/main/java/com/backend/entity/Usuario.java
@@ -0,0 +1,63 @@
+package com.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long id, String email, String senha, String nome) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
