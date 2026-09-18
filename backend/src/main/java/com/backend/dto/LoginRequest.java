4244abb66a27e06944d526cc42f85965964f12ef
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-18 12:12:00 -0300

    Implementando o dto

---
+++ backend/src/main/java/com/backend/dto/LoginRequest.java
@@ -0,0 +1,26 @@
+package com.backend.dto;
+
+public class LoginRequest {
+
+    private String email;
+    private String senha;
+
+    public LoginRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
