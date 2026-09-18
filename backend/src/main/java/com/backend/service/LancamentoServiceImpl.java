57580c0be01edfe4168b415d0392b59fb1a55909
viniciusbatistadev <viniciusbatistabrazoffice@gmail.com>
2026-09-18 11:36:33 -0300

    Implementando os metodos vazios para implementacoes

---
+++ backend/src/main/java/com/backend/service/LancamentoServiceImpl.java
@@ -0,0 +1,33 @@
+package com.backend.service;
+
+import java.util.List;
+
+import com.backend.entity.Lancamento;
+
+public class LancamentoServiceImpl implements LancamentoService {
+
+    @Override
+    public void save(Lancamento lancamento) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public List<Lancamento> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Lancamento findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+    
+}
